35b0185393c12c593327da917e39ab05ab0c5fab
Simeon Nedkov <simeon@ndkv.nl>
2015-09-08 12:53:17 +0200

    add scraping workshop

---
+++ workshop-scraping/readme.md
@@ -0,0 +1,17 @@
+### Configuration
+
+## Install BeautifulSoup 
+
+
+
+
+BeautifulSoup documentation: http://www.crummy.com/software/BeautifulSoup/bs4/doc/#contents-and-children
+
+
+### Common BeautifulSoup operations
+
+
+
+### Hints
+
+- right click on an HTML element -> Inspect element to see its HTML definition
---
+++ workshop-scraping/scrape-example.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+
+with open('source/library.html', 'r') as f:
+	data = BeautifulSoup(f)
+ 
+print data
+
+#print data.title
+#print data.title.string
+
+#print data.title.parent.name
+
+#print data.find_all('div')
+#print data.find(id="book1")
+
+#def extract_books(data):
+#    books_f = open('data/books.csv', 'w')
+#    books_f.write('title,author\n')
+#    
+#    for book in data.findAll(class_="book"):
+#    	title = book.find(class_="title").text
+#    	author = book.find(class_="author").text
+#    
+#    	books_f.write("%s,%s\n" % (title, author))
+#    
+#    books_f.close()
+
+# for child in data.find(id="onloan").children:
+# 	print child
