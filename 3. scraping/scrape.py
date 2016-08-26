2f491d4f28a09e0b4c60783f75a9a8e992d732d7
Simeon Nedkov <simeon@ndkv.nl>
2016-08-26 12:17:49 +0200

    change tabs to spaces

--- 3. scraping/scrape.py
+++ 3. scraping/scrape.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import codecs
 
 with open('sources/windstats.htm', 'r') as f:
-	data = BeautifulSoup(f)
+    data = BeautifulSoup(f)
  
 rows = data.find_all('tr')
 
@@ -12,11 +12,11 @@ turbines_f = codecs.open('results/turbines.csv', 'w', encoding="utf8")
 turbines_f.write('turbine,fabrikant\n')
    
 for row in rows:
-	cells = row.find_all('td')
+    cells = row.find_all('td')
 
-	if cells != []:
-		print cells[0]
+    if cells != []:
+        print cells[0]
 
-		turbines_f.write("%s,%s\n" % (cells[1].string, cells[2].string))
+        turbines_f.write("%s,%s\n" % (cells[1].string, cells[2].string))
 
 turbines_f.close()
