d3b1eac6504082e337195d3c0102ed68bb5f8593
Chris Lucas <chris.lucas@GEODAN.NL>
2017-09-19 12:55:58 +0200

    Changes

--- 3_TwitterAPI/amsterdam_tweets.py
+++ 3_TwitterAPI/amsterdam_tweets.py
@@ -9,49 +9,66 @@ access_token = ''
 access_token_secret = ''
 
 # Create an OAuth authentication object
-auth = 
+auth =
 
 # Create a tweepy API object, using the OAuth object
-api = 
+api =
 
 # Test the connection
 user = api.me()
 print 'Logged in as {}'.format(user.name)
 
-# Search for Amsterdam Twitter ID. Use the reverse_geocode method of the tweepy
-# API object. You will need a latitude and longitude of Amsterdam for this.
-places = api.reverse_geocode(
+# To find the Amsterdam Twitter ID we use the reverse_geocode method of the tweepy
+# API object. We need a latitude and longitude of Amsterdam for this.
+places = api.reverse_geocode(lat=52.362951, lon=4.928977)
+# this returns a list of places associated with the latitude and longitude given.
 # Print the places and search for the amsterdam city place id. You will probably also
 # find a neighborhood, the province of Noord-Holland and the country of the Netherlands
 # in the list of places.
+# We can see that the city of Amsterdam is the first in the list, so we save the first
+# item in the list as a variable.
+amsterdam = places[0]
+# To get the id from this variable we use the folowing command:
+amsterdam_id = amsterdam.id
 
 
 # Create the query (search text) HINT: To search based on a Twitter place ID search for
 # "place:`ID`", replacing `ID` with the ID found of the place.
+# Since we saved the ID in the variable amsterdam_id we need to insert this variable
+# into a string. HINT: Look at the recap to see how to put variables into strings.
 # Check if the query works by entering it in the search bar on the twitter website:
 # https://twitter.com/search-home
 # You should see tweets located in Amsterdam
-query = 
+query =
 
 # Create a tweepy cursor. A cursor is an object that points to other objects, which you
 # can iterate through. In our case the cursor will point to the recent 1000 tweets
-# located in Amsterdam. The cursor is build using tweepy.Cursor(api_method, args),
-# where api_method is any method in the tweepy api object and args are the arguments
+# located in Amsterdam. The cursor is build using tweepy.Cursor(api.method, args),
+# where api.method is any method in the tweepy api object and args are the arguments
 # that need to be passed into said method.
-# HINT: Use the dir or help function in python or the tweepy documentation online to find
-# out which method you need and which argument you have to use to pass in the query.
+# HINT: Use the tweepy documentation online (http://docs.tweepy.org/en/v3.5.0/api.html) to find
+# out which api method you need to search for tweets and which argument you have to use to pass in the query.
 
 
 # To save all tweets with coordinates we loop through all and add the tweets with
 # coordinates to a list.
 
-    # HINT: Print a tweet, where is the location saved?
+
+
+
+for tweet in ...?:
+    # We can convert the tweet to a dictionary using _json
+    tweet_dict = tweet._json
+    # HINT: Print a tweet dictionary, where is the location saved?
     # HINT: Use an if statement to check if the coordinates are present.
+    if ...?:
+        # append the tweet to the lift if it has coordinates. Look at the recap to see
+        # how to append a value to a list
+
 
 
 # Save to a shapefile using the fiona module similar to the previous assignment.
 # Think about which information you want to save to the properties of each point.
-# HINT: print a tweet and have a look which properties are available.
-# Also think about the coordinate system we are using and adjust the crs
-# accordingly if needed.
+# HINT: look in the tweet list and pick a tweet. Look in the dictionary which properties are available.
+# Also think about the coordinate system we are using and adjust the crs accordingly if needed.
 
